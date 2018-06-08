cdd082311f8fbc8ea274267c9809b8ecad27b516
finnishare <finnishare@163.com>
2018-06-08 16:39:46 +0800

    0608

--- js/topology.js
+++ js/topology.js
@@ -10,15 +10,16 @@ var simulation = d3.forceSimulation()
     .force("charge", d3.forceManyBody())
     .force("center", d3.forceCenter(width / 2, height / 2));
 
+
 d3.json('data.json').then(function (graph) {
-    /*  var link = svg.append("g")
-          .attr("class", "links")
-          .selectAll("line")
-          .data(graph.links)
-          .enter().append("line")
-          .attr("stroke-width", function(d) { return Math.sqrt(d.value); });*/
+    var link = svg.append("g")
+        .attr("class", "links")
+        .selectAll("line")
+        .data(graph.links)
+        .enter().append("line");
 
     var node = svg.append("g")
+        .attr("class", "nodes")
         .selectAll("g")
         .data(graph.nodes)
         .enter().append("g")
@@ -29,74 +30,56 @@ d3.json('data.json').then(function (graph) {
             .on("end", dragended));
 
 
-
-    node.filter(function(d) {
-        return d.level===1;
+    node.filter(function (d) {
+        return d.level === 1;
     })
         .append("rect")
-            .attr("class", function (d) {
-                return getClassByLevel(d);
-            })
-            .attr("width", function (d) {
-                return 100;
-            })
-            .attr("height", 30);
-
-    node.append("text")
-        .text(function (d) {
-                return d.value;
-            }
-        ) .attr("class", function (d) {
-        return getClassByLevel(d);
-    }).attr('dy', '1em').attr('text-anchor','middle')
-
-    /*var node = svg.append("g")
-        .attr("class", "nodes")
-        .selectAll("rect")
-        .data(graph.nodes)
-        .enter().append("rect")
         .attr("class", function (d) {
             return getClassByLevel(d);
         })
         .attr("width", function (d) {
             return 100;
-        })
-        .attr("height", 30)
-        .call(d3.drag()
-            .on("start", dragstarted)
-            .on("drag", dragged)
-            .on("end", dragended));
+        });
 
     node.append("text")
         .text(function (d) {
                 return d.value;
-            }.attr('x', 10).attr('y', 10)
-        );*/
+            }
+        ).attr("class", function (d) {
+        return getClassByLevel(d);
+    })
+        .attr('x', '10')
+        .attr('y', '20');
+
 
     simulation
         .nodes(graph.nodes)
         .on("tick", ticked);
+    simulation.force("link")
+        .links(graph.links)
+        .distance(function (d) {
+            return 200;
+        });
 
-    /* simulation.force("link")
-         .links(graph.links);
- */
     function ticked() {
-        /*  link
-              .attr("x1", function(d) { return d.source.x; })
-              .attr("y1", function(d) { return d.source.y; })
-              .attr("x2", function(d) { return d.target.x; })
-              .attr("y2", function(d) { return d.target.y; });
-  */
+        link
+            .attr("x1", function (d) {
+                return d.source.x;
+            })
+            .attr("y1", function (d) {
+                return d.source.y;
+            })
+            .attr("x2", function (d) {
+                return d.target.x;
+            })
+            .attr("y2", function (d) {
+                return d.target.y;
+            });
+
         node
             .attr('transform', function (d) {
-                return 'translate(' + d.x + ',' + d.y + ')'
+                return 'translate(' + (d.x - getLetterOffset(d)) + ',' + (d.y - 15) + ')'
             })
-            .attr("x", function (d) {
-                return d.x;
-            })
-            .attr("y", function (d) {
-                return d.y;
-            });
     }
 }, function (error) {
     console.error(error);
@@ -112,6 +95,19 @@ function getClassByLevel(d) {
             return 'level2';
         default :
             return 'level2';
+    }
+}
+
+function getLetterOffset(d) {
+    switch (d.level) {
+        case 0 :
+            return (d.value.length || 1) * 13 / 2;
+        case 1 :
+            return 100 / 2;
+        case 2 :
+            return (d.value.length || 1) * 13 / 2;
+        default :
+            return (d.value.length || 1) * 13 / 2;
     }
 }
 
